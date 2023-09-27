94b36ec411bb7c88226da82596d9b3517fa0cb03
Justnoones <yanw97841@gmail.com>
2023-09-27 10:35:46 +0630

    nike-project-clone

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import Hero from './Hero';
+
+export {
+    PopularProducts,
+    Footer,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    Hero
+}
